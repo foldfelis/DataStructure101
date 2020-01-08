38d67d25aa457f858387ba8ca9ee729e7eb26cac
JingYu Ning <foldfelis@gmail.com>
2020-01-09 00:05:56 +0800

    Implement Stack

---
+++ stack/src/stack.jl
@@ -0,0 +1,24 @@
+import Base: show, push!
+
+mutable struct Stack
+    data::Vector
+    Stack(d) = new([d, nothing])
+end
+
+function show(io::IO, s::Stack)
+    print("Stack([")
+    for i in s.data
+        if i != nothing
+            print("$(i), ")
+        end
+    end
+    print("end])")
+end
+
+function pop!(s::Stack)
+    deleteat!(s.data, 1)
+end
+
+function push!(s::Stack, d)
+    pushfirst!(s.data, d)
+end
