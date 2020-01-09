cb0af603afdcf8a8f58c7158c0e6c7bad5645e08
JingYu Ning <foldfelis@gmail.com>
2020-01-09 18:59:08 +0800

    Implement Circular List

--- CircularList/src/CircularList.jl
+++ CircularList/src/CircularList.jl
@@ -1,5 +1,5 @@
 
-import Base: show, push!
+import Base: show, push! ,delete!
 
 mutable struct Node
     data::Union{Any, Nothing}
@@ -23,4 +23,82 @@ function show(io::IO, node::Node)
     else
         print("Nothing")
     end
+end
+
+mutable struct CircularList
+    current_node::Node
+
+    function CircularList(data)
+        node = Node(data)
+        node.next = node
+        node.prev = node
+
+        return new(node)
+    end
+end
+
+function show(io::IO, cl::CircularList)
+    print("CircularList([..., ")
+
+    current = cl.current_node
+    while current.next != cl.current_node
+        print("$(current.data), ")
+        current = current.next
+    end
+    print("$(current.data), ...])")
+end
+
+function movePtr(cl::CircularList, n)
+    if n > 0
+        while n != 0
+            cl.current_node = cl.current_node.next
+            n -= 1
+        end
+    elseif n < 0
+        while n != 0
+            cl.current_node = cl.current_node.prev
+            n += 1
+        end
+    end
+end
+
+function push!(cl::CircularList, data)
+    node = Node(data)
+    current = cl.current_node
+
+    node.next = current
+    node.prev = current.prev
+    current.prev = node
+    node.prev.next = node
+
+    cl.current_node = node
+end
+
+function push!(cl::CircularList, data, i::Int64)
+    if i > 0    # To match the Jilia index system
+        i -= 1
+    end
+    movePtr(cl, i)
+
+    push!(cl, data)
+end
+
+function delete!(cl::CircularList)
+    current = cl.current_node
+
+    current.next.prev = current.prev
+    current.prev.next = current.next
+
+    cl.current_node = current.next
+
+    return current.data
+end
+
+function delete!(cl::CircularList, i::Int64)
+    if i > 0    # To match the Jilia index system
+        i -= 1
+    end
+    movePtr(cl, i)
+
+    return delete!(cl)
 end
--- CircularList/test/CircularList.jl
+++ CircularList/test/CircularList.jl
@@ -1,7 +1,50 @@
 using Test
 include("../src/CircularList.jl")
 
+#############
+# Node Test #
+#############
+
 node = Node("This is a node")
 println(node)
 type_result = @test typeof(node) == Node
 println(type_result)
+
+#######################
+# Circular List  Test #
+#######################
+
+cl = CircularList(0)
+println(cl)
+type_result = @test typeof(cl) == CircularList
+println(type_result)
+
+for i = 1:20
+    push!(cl, i)
+end
+println(cl)
+
+current_data = delete!(cl)
+println("Deleted data: $(current_data)")
+println(cl)
+
+
+movePtr(cl, 101)
+println(cl)
+
+movePtr(cl, -101)
+println(cl)
+
+push!(cl, "This is 101", 101)
+println(cl)
+
+push!(cl, "This is -101", -43)
+println(cl)
+
+current_data = delete!(cl, 48)
+println("Deleted data: $(current_data)")
+println(cl)
+
+current_data = delete!(cl, -21)
+println("Deleted data: $(current_data)")
+println(cl)
