2f02dbc48a5b70dc09b7e76404a2588cfbd09434
JingYu Ning <foldfelis@gmail.com>
2020-01-09 17:38:36 +0800

    Implement `delete!` and `insert`

--- LinkedList/src/LinkedList.jl
+++ LinkedList/src/LinkedList.jl
@@ -1,7 +1,7 @@
-import Base: show, push!, +
+import Base: show, push!, pushfirst!
 
 mutable struct Node
-    data::Union{Number, Nothing}
+    data::Union{Any, Nothing}
     prev::Union{Node, Nothing}
     next::Union{Node, Nothing}
 
@@ -47,7 +47,7 @@ function tail(ll::LinkedList)
     return node
 end
 
-function len(ll::LinkedList)
+function length(ll::LinkedList)
     node = head(ll)
     i = 0
     while node != nothing
@@ -60,7 +60,7 @@ end
 
 function show(io::IO, ll::LinkedList)
     println("LinkedList(")
-    println("\t There are $(len(ll)) node in the LinkedList")
+    println("\t There are $(length(ll)) node in the LinkedList")
 
     print("\t [")
     node = head(ll)
@@ -82,6 +82,33 @@ function show(io::IO, ll::LinkedList)
     print(")")
 end
 
+function movePtr(ll::LinkedList, n)
+    if n > 0
+        while n != 0
+            if ll.current_node.next == nothing
+                break
+            end
+            ll.current_node = ll.current_node.next
+            n -= 1
+        end
+    elseif n < 0
+        while n != 0
+            if ll.current_node.prev == nothing
+                break
+            end
+            ll.current_node = ll.current_node.prev
+            n += 1
+        end
+    end
+end
+
+function movePtr2head(ll::LinkedList)
+    ll.current_node = head(ll)
+end
+
+function movePtr2tail(ll::LinkedList)
+    ll.current_node = tail(ll)
+end
 
 function push!(ll::LinkedList, data)
     node = Node(data)
@@ -101,22 +128,62 @@ function push!(ll::LinkedList, data)
     ll.current_node = node
 end
 
-function movePtr(ll::LinkedList, n)
-    if n > 0
-        while n != 0
-            if ll.current_node.next == nothing
-                break
-            end
-            ll.current_node = ll.current_noce.next
-            n -= 1
-        end
-    elseif n < 0
-        while n != 0
-            if ll.current_node.prev == nothing
-                break
-            end
-            ll.current_node = ll.current_node.prev
-            n += 1
-        end
+function pushfirst!(ll::LinkedList, data)
+    node = Node(data)
+    current_node = head(ll)
+
+    node.next = current_node
+    current_node.prev = node
+
+    ll.current_node = head(ll)
+end
+
+function insert!(ll::LinkedList, data, i::Int64)
+    if i > 1
+        movePtr2head(ll)
+        movePtr(ll, i-2)
+        push!(ll, data)
+    elseif i < 0
+        movePtr2tail(ll)
+        movePtr(ll, i+1)
+        push!(ll, data)
+    else
+        pushfirst!(ll, data)
+    end
+end
+
+function delete!(ll::LinkedList)
+    current_node = ll.current_node
+
+    if current_node.prev != nothing
+        current_node.prev.next = current_node.next
+    end
+    if current_node.next != nothing
+        current_node.next.prev = current_node.prev
+    end
+
+    if current_node.next != nothing
+        ll.current_node = current_node.next
+    else
+        ll.current_node = current_node.prev
+    end
+
+    return current_node.data
+end
+function delete!(ll::LinkedList, i::Int64)
+    if i > 1
+        movePtr2head(ll)
+        movePtr(ll, i-2)
+        current_data = delete!(ll)
+        return current_data
+    elseif i < 0
+        movePtr2tail(ll)
+        movePtr(ll, i+1)
+        current_data = delete!(ll)
+        return current_data
+    else
+        movePtr2head(ll)
+        current_data = delete!(ll)
+        return current_data
     end
 end
--- LinkedList/test/LinkedList.jl
+++ LinkedList/test/LinkedList.jl
@@ -20,6 +20,8 @@ println(ll)
 type_result = @test typeof(ll) == LinkedList
 println("Type test result: $(type_result)\n")
 
+println("\n* Push Test\n")
+
 for i=1:20
     push!(ll, i)
 end
@@ -28,4 +30,48 @@ println(ll)
 movePtr(ll, -5)
 println(ll)
 push!(ll, 100)
+println(ll)
+
+println("\n* Delete Current Node Test\n")
+
+movePtr(ll, 3)
+println(ll)
+current_data = delete!(ll)
+println("Deleted data: $(current_data)")
+println(ll)
+
+println("\n* Move Test\n")
+
+movePtr2head(ll)
+println(ll)
+
+movePtr2tail(ll)
+println(ll)
+
+println("\n* Pushfirst Test\n")
+pushfirst!(ll, 123)
+println(ll)
+
+println("\n* Insert Test\n")
+
+insert!(ll, 5987, 5)
+println(ll)
+
+insert!(ll, 8745, -5)
+println(ll)
+
+insert!(ll, "This is 0", 0)
+println(ll)
+insert!(ll, "This is 1", 1)
+println(ll)
+insert!(ll, "This is -1", -1)
+println(ll)
+
+println("\n* Delete Test\n")
+
+delete!(ll, 0)
+println(ll)
+delete!(ll, 1)
+println(ll)
+delete!(ll, -1)
 println(ll)
