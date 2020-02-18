db46570ed9efe8aa44badd927f6d5a0f0acf01c0
JingYu Ning <foldfelis@gmail.com>
2020-02-18 15:26:33 +0800

    Implement UnionFind

---
+++ src/unionfind.jl
@@ -0,0 +1,14 @@
+makeset(n::Int64) = -ones(Int64, n)
+
+parent(set::Vector{Int64}, element::Int64) = set[element]
+
+function find(set::Vector{Int64}, element::Int64)
+    p = parent(set, element)
+    if p == -1 return element end
+    find(set, p)
+end
+
+function union!(set::Vector{Int64}, a::Int64, b::Int64)
+    root = find(set, a)
+    set[b] = root
+end
