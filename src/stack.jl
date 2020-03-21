a3ce2b20d3781a31f98539326440d62d351d37ad
JingYu Ning <foldfelis@gmail.com>
2020-03-22 01:06:53 +0800

    stack

--- src/stack.jl
+++ src/stack.jl
@@ -1,9 +1,13 @@
+import Base
+
+export Stack
+
 mutable struct Stack{T}
     data::Vector{T}
     Stack{T}(d::T) where T = new(T[d])
 end
 
-function show(io::IO, s::Stack)
+function Base.show(io::IO, s::Stack)
     print(io, "Stack([")
     for (i, x) in enumerate(s.data)
         print(io, "$(x)")
@@ -14,6 +18,6 @@ function show(io::IO, s::Stack)
     print(io, "])")
 end
 
-pop!(s::Stack) = deleteat!(s.data, 1)
+Base.pop!(s::Stack) = deleteat!(s.data, 1)
 
-push!(s::Stack, d) = pushfirst!(s.data, d)
+Base.push!(s::Stack, d) = pushfirst!(s.data, d)
