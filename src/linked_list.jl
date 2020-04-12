c1c7dbe0cc34c2d3feeacc0141326e72982f2fd3
JingYu Ning <foldfelis@gmail.com>
2020-04-12 18:21:53 +0800

    insert! delete!

--- src/linked_list.jl
+++ src/linked_list.jl
@@ -62,65 +62,6 @@ function Base.show(io::IO, ll::LinkedList{T}) where T
     print(io, "$(node)])")
 end
 
-function Base.push!(ll::LinkedList, data::T) where T
-    node = ListNode{T}(data)
-
-    if is_empty(ll)
-        ll.head_node = node
-        ll.length += 1
-        return
-    end
-
-    tail_node = tail(ll)
-    tail_node.next = node
-    node.prev = tail_node
-
-    ll.length += 1
-end
-
-function Base.pushfirst!(ll::LinkedList, data::T) where T
-    node = ListNode{T}(data)
-    head_node = head(ll)
-    ll.head_node = node
-
-    if is_empty(ll)
-        ll.length += 1
-        return
-    end
-
-    head_node.prev = node
-    node.next = head_node
-
-    ll.length += 1
-end
-
-function Base.pop!(ll::LinkedList)
-    is_empty(ll) && throw(BoundsError("List is empty."))
-
-    tail_node = tail(ll)
-    if !(tail_node.prev isa NullNode)
-        tail_node.prev.next = NullNode()
-    else
-        ll.head_node = NullNode()
-    end
-
-    ll.length -= 1
-
-    return value(tail_node)
-end
-
-function Base.popfirst!(ll::LinkedList)
-    is_empty(ll) && throw(BoundsError("List is empty."))
-
-    head_node = head(ll)
-    if !(head_node.next isa NullNode) head_node.next.prev = NullNode() end
-
-    ll.head_node = head_node.next
-    ll.length -= 1
-
-    return value(head_node)
-end
-
 function n_node(ll::LinkedList, n::Int64)
     if is_empty(ll) throw(BoundsError("List is empty.")) end
     if ll.length < n throw(BoundsError("Out of boundary.")) end
@@ -139,11 +80,67 @@ function Base.setindex!(ll::LinkedList{T}, data::T, i::Int64) where T
     n_node(ll, i).data = data
 end
 
+function Base.pushfirst!(ll::LinkedList, data::T) where T
+    node = ListNode{T}(data)
+    head_node = head(ll)
+    ll.head_node = node
 
-# function Base.insert!(ll::LinkedList, data::T, i::Int64) where T
-#     # TODO:
-# end
+    if !is_empty(ll)
+        head_node.prev = node
+        node.next = head_node
+    end
 
-# function Base.delete!(ll::LinkedList, i::Int64)
-#     # TODO
-# end
+    ll.length += 1
+end
+
+function Base.insert!(ll::LinkedList, data::T, i::Int64) where T
+    if i < 2
+        Base.pushfirst!(ll, data)
+    else
+        node = ListNode{T}(data)
+        current_node = n_node(ll, i-1)
+        next_node = next(current_node)
+
+        node.prev = current_node
+        current_node.next = node
+        node.next = next_node
+        !(next_node isa NullNode) && (next_node.prev = node)
+
+        ll.length += 1
+    end
+end
+
+function Base.push!(ll::LinkedList, data::T) where T
+    is_empty(ll) ? Base.pushfirst!(ll, data) : Base.insert!(ll, data, ll.length+1)
+end
+
+function Base.popfirst!(ll::LinkedList)
+    is_empty(ll) && throw(BoundsError("List is empty."))
+
+    head_node = head(ll)
+    if !(head_node.next isa NullNode) head_node.next.prev = NullNode() end
+
+    ll.head_node = next(head_node)
+    ll.length -= 1
+
+    return value(head_node)
+end
+
+function Base.delete!(ll::LinkedList, i::Int64)
+    if i < 2
+        return Base.popfirst!(ll)
+    else
+        current_node = n_node(ll, i)
+        prev_node = prev(current_node)
+        next_node = next(current_node)
+
+        !(next_node isa NullNode) && (next_node.prev = prev_node)
+        !(prev_node isa NullNode) && (prev_node.next = next_node)
+
+        ll.length -= 1
+
+        return value(current_node)
+    end
+end
+
+Base.pop!(ll::LinkedList) = Base.delete!(ll, ll.length)
--- test/linked_list.jl
+++ test/linked_list.jl
@@ -2,47 +2,82 @@ using Test
 using DataStructure101
 const DS = DataStructure101
 
+@testset "Test Nodes" begin
+    node = DS.NullNode()
+    @test repr(node) == ""
+
+    node = DS.ListNode{Int64}(10)
+    @test repr(node) == "10"
+end
+
 @testset "Test LinkedList" begin
     # Initial a LinkedList
     T_e = Int64
     T = DS.LinkedList{T_e}
     ll = T()
-
     @test length(ll) == 0
     @test repr(ll) == "LinkedList{$T_e}([])"
 
+    # Push Test
     for i=1:5
         push!(ll, i)
         @test ll[i] == i
     end
-
     @test repr(ll) == "LinkedList{$T_e}([1, 2, 3, 4, 5])"
 
+    # Popfirst Test
     for i=1:5
         @test popfirst!(ll) == i
     end
-
     @test repr(ll) == "LinkedList{$T_e}([])"
 
+    # Pushfirst Test
     for i=6:10
         pushfirst!(ll, i)
         @test ll[1] == i
     end
-
     @test repr(ll) == "LinkedList{$T_e}([10, 9, 8, 7, 6])"
 
+    # Pop Test
     for i=6:10
         @test pop!(ll) == i
     end
-
     @test repr(ll) == "LinkedList{$T_e}([])"
 
     # Setindex Test
     push!(ll, 1)
-    ll[1] = 10
-    @test ll[1] == 10
-    @test repr(ll) == "LinkedList{$T_e}([10])"
+    ll[1] = 30
+    @test ll[1] == 30
+    @test repr(ll) == "LinkedList{$T_e}([30])"
 
     # Insert Test
+    ll = T()
+    insert!(ll, 30, 1)
+    @test ll[1] == 30
+    @test repr(ll) == "LinkedList{$T_e}([30])"
 
+    insert!(ll, 10, 1)
+    @test ll[1] == 10
+    @test repr(ll) == "LinkedList{$T_e}([10, 30])"
+
+    insert!(ll, 20, 2)
+    @test ll[2] == 20
+    @test repr(ll) == "LinkedList{$T_e}([10, 20, 30])"
+
+    insert!(ll, 40, 4)
+    @test ll[4] == 40
+    @test repr(ll) == "LinkedList{$T_e}([10, 20, 30, 40])"
+
+    # Delete Test
+    @test delete!(ll, 1) == 10
+    @test repr(ll) == "LinkedList{$T_e}([20, 30, 40])"
+
+    @test delete!(ll, 2) == 30
+    @test repr(ll) == "LinkedList{$T_e}([20, 40])"
+
+    @test delete!(ll, 2) == 40
+    @test repr(ll) == "LinkedList{$T_e}([20])"
+
+    @test delete!(ll, 1) == 20
+    @test repr(ll) == "LinkedList{$T_e}([])"
 end
