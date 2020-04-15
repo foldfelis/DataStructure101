da1c1fdef6364f4055e0dad8848dfeda9cc78b6b
JingYu Ning <foldfelis@gmail.com>
2020-04-15 20:17:10 +0800

    increase coverage

--- test/min_max_heap.jl
+++ test/min_max_heap.jl
@@ -25,4 +25,39 @@ const DS = DataStructure101
     @test maximum(heap) == 30
 
     @test sort!(heap) == collect(1:30)
+
+    for i=1:30
+        push!(heap, i)
+    end
+    DS.build!(heap)
+
+    @test maximum(heap) == 30
+    @test minimum(heap) == 1
+
+    @test DS.pop_max!(heap) == 30
+    @test DS.pop_min!(heap) == 1
+
+    DS.push_bubble!(heap, 1)
+    @test minimum(heap) == 1
+
+    DS.push_bubble!(heap, 30)
+    @test maximum(heap) == 30
+
+    @test sort!(heap) == collect(1:30)
+
+    for i=1:30
+        DS.push_bubble!(heap, i)
+    end
+
+    @test maximum(heap) == 30
+    @test minimum(heap) == 1
+    @test sort!(heap) == collect(1:30)
+
+    for i=30:-1:1
+        DS.push_bubble!(heap, i)
+    end
+
+    @test maximum(heap) == 30
+    @test minimum(heap) == 1
+    @test sort!(heap) == collect(1:30)
 end
--- test/priority_queue.jl
+++ test/priority_queue.jl
@@ -7,6 +7,10 @@ const DS = DataStructure101
     for i = 1:26
         push!(pq, '`'+i)
     end
+    @test repr(pq) == "PriorityQueue{Char}([" *
+        "'a', 'b', 'c', 'd', 'e', 'f', 'g', " *
+        "'h', 'i', 'j', 'k', 'l', 'm', 'n', " *
+        "'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']"
     @test minimum(pq) == (1 => 'a')
     @test maximum(pq) == (26 => 'z')
     @test DS.pop_min!(pq) == 'a'
