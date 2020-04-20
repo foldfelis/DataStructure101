9706942a2df0ac1bd883fe9b966f12fbcdc9f40c
JingYu Ning <foldfelis@gmail.com>
2020-04-20 15:25:21 +0800

    Implement UndirectedWeightedAdjacencyList

--- src/graph.jl
+++ src/graph.jl
@@ -1,5 +1,7 @@
-export AbstractGraph, AdjacencyMatrix, WeightedAdjacencyMatrix, AdjacencyList, Graph
-export nv, ne, relate!, neighbor, weight, degree
+export AbstractGraph, Graph
+export AdjacencyMatrix, WeightedAdjacencyMatrix
+export AdjacencyList
+export nv, ne, relate!, unrelate!, neighbor, weight, degree
 export probability, random_walk
 
 abstract type AbstractGraph end
@@ -71,35 +73,110 @@ mutable struct AdjacencyList <: AbstractGraph
     AdjacencyList(n::Int64) = new([Int[] for i=1:n], n)
 end
 
-# TODO: WeightedAdjacencyList
+mutable struct UndirectedWeightedAdjacencyList{T} <: AbstractGraph
+    relation::Vector{Vector{Int64}}
+    weight::Vector{Vector{T}}
+    n_vertices::Int64
 
-Base.getindex(g::AdjacencyList, i::Int64) = 0 < i < g.n_vertices ? i : -1
+    function UndirectedWeightedAdjacencyList{T}(n::Int64) where T
+        return new(
+            [Int[] for i=1:n],
+            [T[] for i=1:n],
+            n
+        )
+    end
+end
 
-nv(g::AdjacencyList) = g.n_vertices
+function Base.getindex(
+    g::Union{AdjacencyList, UndirectedWeightedAdjacencyList},
+    i::Int64
+)
+    return 0 < i < g.n_vertices ? i : -1
+end
 
-ne(g::AdjacencyList) = sum([length(g.relation[v]) for v in 1:(g.n_vertices)])
+nv(g::Union{AdjacencyList, UndirectedWeightedAdjacencyList}) = g.n_vertices
 
-neighbor(g::AdjacencyList, v::Int64) = g.relation[v]
+function ne(g::Union{AdjacencyList, UndirectedWeightedAdjacencyList})
+    return sum([length(g.relation[v]) for v in 1:(g.n_vertices)])
+end
 
-degree(g::AdjacencyList,  v::Int64) = length(g.relation[v])
+function neighbor(
+    g::Union{AdjacencyList, UndirectedWeightedAdjacencyList},
+    v::Int64
+)
+    return g.relation[v]
+end
+
+function degree(
+    g::Union{AdjacencyList, UndirectedWeightedAdjacencyList},
+    v::Int64
+)
+    return length(g.relation[v])
+end
 
 function Base.show(io::IO, g::AdjacencyList)
     print(io, "AdjacencyList(")
     for i=1:g.n_vertices
-        print(io, "$i($(g.relation[i]))")
+        print(io, "$i[")
+        for j=g.relation[i]
+            print(io, "$j, ")
+        end
+        print(io, "], ")
     end
     print(io, ")")
 end
 
-function relate!(g::AdjacencyList, v1::Int64, v2::Int64)
+relate!(g::AdjacencyList, v1::Int64, v2::Int64) = push!(g.relation[v1], v2)
+
+function unrelate!(g::AdjacencyList, v1::Int64, v2::Int64)
+    deleteat!(g.relation[v1], findall(x->x==v2, g.relation[v1]))
+end
+
+function Base.show(io::IO, g::UndirectedWeightedAdjacencyList{T}) where T
+    print(io, "UndirectedWeightedAdjacencyList{$T}(")
+    for i=1:g.n_vertices
+        print(io, "$i[")
+        for (index, j)=enumerate(g.relation[i])
+            print(io, "$j($(g.weight[i][index])), ")
+        end
+        print(io, "], ")
+    end
+    print(io, ")")
+end
+
+function relate!(
+    g::UndirectedWeightedAdjacencyList{T},
+    v1::Int64,
+    v2::Int64,
+    w::T
+) where T
     push!(g.relation[v1], v2)
+    push!(g.relation[v2], v1)
+    push!(g.weight[v1], w)
+    push!(g.weight[v2], w)
+end
+
+function unrelate!(
+    g::UndirectedWeightedAdjacencyList{T},
+    v1::Int64,
+    v2::Int64,
+) where T
+    indexes = findall(x->x==v2, g.relation[v1])
+    deleteat!(g.relation[v1], indexes)
+    deleteat!(g.weight[v1], indexes)
+    indexes = findall(x->x==v1, g.relation[v2])
+    deleteat!(g.relation[v2], indexes)
+    deleteat!(g.weight[v2], indexes)
 end
 
 function Graph(n::Int64, representation::Symbol; random::Bool=true, T=Float64)
-    (representation == :matrix) &&
-        (return AdjacencyMatrix(n))
-    (representation == :list) &&
-        (return AdjacencyList(n))
-    (representation == :w_matrix) &&
-        (return WeightedAdjacencyMatrix{T}(n=n, random=random))
+    if representation == :matrix
+        return AdjacencyMatrix(n)
+    elseif representation == :list
+        return AdjacencyList(n)
+    elseif representation == :w_matrix
+        return WeightedAdjacencyMatrix{T}(n=n, random=random)
+    elseif representation == :udw_list
+        return UndirectedWeightedAdjacencyList{T}(n)
+    end
 end
--- test/graph.jl
+++ test/graph.jl
@@ -87,7 +87,53 @@ const DS = DataStructure101
         @test DS.neighbor(g, 10) == [9, 8]
 
         @test repr(g) == "AdjacencyList(" *
-            "1(Int64[])2(Int64[])3(Int64[])4(Int64[])5(Int64[])" *
-            "6([1])7([3, 2])8([5, 4])9([7, 6])10([9, 8]))"
+            "1[], 2[], 3[], 4[], 5[], " *
+            "6[1, ], 7[3, 2, ], 8[5, 4, ], 9[7, 6, ], 10[9, 8, ], )"
+
+        DS.unrelate!(g, 6, 1)
+        @test repr(g) == "AdjacencyList(" *
+            "1[], 2[], 3[], 4[], 5[], " *
+            "6[], 7[3, 2, ], 8[5, 4, ], 9[7, 6, ], 10[9, 8, ], )"
+    end
+
+    @testset "UndirectedWeightedAdjacencyList" begin
+        g = DS.Graph(10, :udw_list)
+        @test g isa DS.UndirectedWeightedAdjacencyList
+        @test g isa DS.AbstractGraph
+
+        @test g[8] == 8
+        @test g[11] == -1
+
+        DS.relate!(g, 10, 9, 9.0)
+        DS.relate!(g, 10, 8, 8.0)
+
+        DS.relate!(g, 9, 7, 7.0)
+        DS.relate!(g, 9, 6, 6.0)
+        DS.relate!(g, 8, 5, 5.0)
+        DS.relate!(g, 8, 4, 4.0)
+
+        DS.relate!(g, 7, 3, 3.0)
+        DS.relate!(g, 7, 2, 2.0)
+        DS.relate!(g, 6, 1, 1.0)
+
+        @test DS.nv(g) == 10
+        @test DS.ne(g) == 18
+        @test DS.degree(g, 8) == 3
+        @test DS.degree(g, 6) == 2
+
+        @test DS.neighbor(g, 10) == [9, 8]
+
+        @test repr(g) == "UndirectedWeightedAdjacencyList{Float64}(" *
+            "1[6(1.0), ], 2[7(2.0), ], 3[7(3.0), ], 4[8(4.0), ], " *
+            "5[8(5.0), ], 6[9(6.0), 1(1.0), ], " *
+            "7[9(7.0), 3(3.0), 2(2.0), ], 8[10(8.0), 5(5.0), 4(4.0), ], " *
+            "9[10(9.0), 7(7.0), 6(6.0), ], 10[9(9.0), 8(8.0), ], )"
+
+        DS.unrelate!(g, 6, 1)
+        @test repr(g) == "UndirectedWeightedAdjacencyList{Float64}(" *
+            "1[], 2[7(2.0), ], 3[7(3.0), ], 4[8(4.0), ], " *
+            "5[8(5.0), ], 6[9(6.0), ], " *
+            "7[9(7.0), 3(3.0), 2(2.0), ], 8[10(8.0), 5(5.0), 4(4.0), ], " *
+            "9[10(9.0), 7(7.0), 6(6.0), ], 10[9(9.0), 8(8.0), ], )"
     end
 end
